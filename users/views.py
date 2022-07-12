189cb024dd7dfca5d897c59c5807e7c3a2262d6e
felipe <felipesindeaux@outlook.com>
2022-07-12 18:35:09 -0300

    feat: login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView, Response, status
 
-# Create your views here.
+from .serializers import LoginSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(
+            username=serializer.validated_data['email'],
+            password=serializer.validated_data['password']
+        )
+
+        if user:
+            token = Token.objects.get_or_create(user=user)[0]
+
+            return Response({'token': token.key})
+
+        return Response({"detail": 'invalid email or password'}, status.HTTP_401_UNAUTHORIZED)
