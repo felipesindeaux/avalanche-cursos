4930424955626b671e89c1f4518c9884d1b9026c
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-07-13 10:47:57 -0300

    fix: create course view with relation of categories

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("courses/", views.CreateListCourseView.as_view()),
+    path("courses/me", views.ListTeacherCoursesView.as_view()),
     path("courses/<pk>/", views.UpdateListCourseView.as_view()),
-    path("/courses/activate/<pk>/", views.ActivateCourseView.as_view()),
-    path("/courses/deactivate/<pk>/", views.DeactivateCourseView.as_view())
+    path("courses/activate/<pk>/", views.ActivateCourseView.as_view()),
+    path("courses/deactivate/<pk>/", views.DeactivateCourseView.as_view())
 ]
