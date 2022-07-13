3e717f7f70b160938b0782b30cf14ee0d1dcac16
felipe <felipesindeaux@outlook.com>
2022-07-13 11:14:26 -0300

    fix: users update serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from .models import User
+
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +12,7 @@ class AccountSerializer(serializers.ModelSerializer):
             "email",
             "is_teacher",
             "password"
-            ]
+        ]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
@@ -20,10 +22,12 @@ class AccountSerializer(serializers.ModelSerializer):
 
         return User.objects.create_user(**validated_data)
 
+
 class LoginSerializer(serializers.Serializer):
 
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
 
 class UpdateAccountSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,10 +39,11 @@ class UpdateAccountSerializer(serializers.ModelSerializer):
             "is_teacher",
             "password",
             "is_active"
-            ]
+        ]
         extra_kwargs = {"password": {"write_only": True}}
         read_only_fields = [
             "is_teacher",
+            "is_active"
         ]
 
 
@@ -53,7 +58,7 @@ class UpdateStatusAccountSerializer(serializers.ModelSerializer):
             "is_teacher",
             "password",
             "is_active"
-            ]
+        ]
         read_only_fields = [
             "id",
             "name",
