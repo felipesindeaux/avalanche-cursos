d3274511f95f6fb102750748f3eaf9dec239eab3
felipe <felipesindeaux@outlook.com>
2022-07-14 17:02:37 -0300

    fix: url kwargs

--- courses/urls.py
+++ courses/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("courses/<pk>/", views.RetrieveUpdateDestroyView.as_view()),
     path("courses/buy/<course_id>/", views.BuyCoursesView.as_view()),
     path("courses/complete/<course_id>/", views.CompletCoursesView.as_view()),
-    path("courses/activate/<course_id>/", views.ActivateCourseView.as_view()),
+    path("courses/activate/<pk>/", views.ActivateCourseView.as_view()),
     path("courses/deactivate/<pk>/", views.DeactivateCourseView.as_view()),
 ]
