3d0bbbecc6526d7ace67e614b2f96b64cb6e9a34
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-07-16 21:10:37 -0300

    fix: courses tests

--- categories/tests/tests_model.py
+++ categories/tests/tests_model.py
@@ -6,12 +6,12 @@ from django.test import TestCase
 class CategoryModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Category.objects.create(
+        cls.category_create = Category.objects.create(
             name="test_category",
         )
 
     def test_first_name_max_length(self):
-        category = Category.objects.get(id=1)
+        category = Category.objects.get(id=self.category_create.id)
 
         max_length = category._meta.get_field("name").max_length
 
