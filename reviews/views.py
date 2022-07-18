ea3285180cffb12720d037dc9af3ce4545f7b3d2
leonardoliska <leo.liska@hotmail.com>
2022-07-18 13:43:35 -0300

    refactor: remove not used url params and fix invalid uuid bug

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import get_object_or_404
+from courses.models import Course
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from utils import get_object_or_404, validate_uuid
 
 from reviews.models import Review
-from reviews.serializers import ReviewSerializer
-from rest_framework.authentication import TokenAuthentication
-from courses.models import Course
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from reviews.permissions import IsReviewOwner
+from reviews.serializers import ReviewSerializer
 
 # Create your views here.
+
 
 class GetOrCreateReviewView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
@@ -19,10 +20,11 @@ class GetOrCreateReviewView(generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
 
     def perform_create(self, serializer):
-       course_id = self.kwargs.get("course_id")
-       course = get_object_or_404(Course, pk=course_id)
-       
-       serializer.save(user=self.request.user, course=course)
+        uuid = validate_uuid(self.kwargs["course_id"])
+
+        course = get_object_or_404(Course, "Course not found", id=uuid)
+
+        serializer.save(user=self.request.user, course=course)
 
     def get_queryset(self):
         course_id = self.kwargs.get("course_id")
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .get_object_or_404 import get_object_or_404
+from .validate_uuid import validate_uuid
