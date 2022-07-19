81b01e3296c8bf7adcb15c4518cf15ab2affc088
felipe <felipesindeaux@outlook.com>
2022-07-19 12:04:59 -0300

    refactor: reviews linter

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -20,7 +20,8 @@ class StudentHaveCourse(permissions.BasePermission):
             return False
 
         course_id = view.kwargs.get("course_id")
-        course = get_object_or_404(Course, detail="Course not found", pk=course_id)
+        course = get_object_or_404(
+            Course, detail="Course not found", pk=course_id)
         try:
             Student.objects.get(course=course, student=request.user)
             return True
