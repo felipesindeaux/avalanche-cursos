b6ab46eb19004c8fae69b5b79115aef902de60c8
felipe <felipesindeaux@outlook.com>
2022-07-19 11:54:27 -0300

    refactor: answers linter

--- answers/tests/tests_model.py
+++ answers/tests/tests_model.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+from datetime import datetime as dt
+
 from answers.models import Answer
 from categories.models import Category
-from users.models import User
+from django.test import TestCase
 from questions.models import Question
-from datetime import datetime as dt
+from users.models import User
 
 
 class AnswerTest(TestCase):
@@ -17,31 +18,31 @@ class AnswerTest(TestCase):
             "title": "Titulo",
             "description": "Description",
         }
-        
-        cls.question = Question.objects.create(**cls.question_data, user=cls.user)
+
+        cls.question = Question.objects.create(
+            **cls.question_data, user=cls.user)
 
         Category.objects.create(name="TEST")
         cls.category = Category.objects.all()
         cls.question.categories.set(cls.category)
         cls.question.save()
-        
 
         cls.answer_data = {
             "content": "Resposta Teste"
         }
 
-        cls.content="Resposta Teste"
-        
-        
+        cls.content = "Resposta Teste"
 
     def test_onwer_of_answer(self):
-        answer = Answer.objects.create(**self.answer_data, user=self.user, question=self.question)
+        answer = Answer.objects.create(
+            **self.answer_data, user=self.user, question=self.question)
         answer.save()
         self.assertIn("user_id", answer.__dict__)
         self.assertEqual(self.user.id, answer.user_id)
 
     def test_answer_has_information_fields(self):
-        answer = Answer.objects.create(**self.answer_data, user=self.user, question=self.question)
+        answer = Answer.objects.create(
+            **self.answer_data, user=self.user, question=self.question)
         answer.save()
         date_now = dt.strftime(dt.now(), "%D")
         date_model = dt.strftime(answer.date_published, "%D")
